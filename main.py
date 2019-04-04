e8e8ec7c5bee4c82420755677f5132cf0e129e75
Kenny Baxter <kenny.baxter@outlook.com>
2019-04-04 19:01:25 +0100

    Sketch out some initial test cases

---
+++ main.py
@@ -0,0 +1,2 @@
+def splitArray(array, slices):
+    return []
---
+++ tests.py
@@ -0,0 +1,44 @@
+import unittest
+from main import splitArray
+
+class SplitArrayTests(unittest.TestCase):
+
+    def testNoArray(self):
+        self.assertRaises(ValueError, splitArray(None, 10))
+
+    def testEmptyArray(self):
+        result = splitArray([], 10)
+        self.assertEqual(result, [])
+
+    def testBadSlicesArgument(self):
+        self.assertRaises(ValueError, splitArray([0,1,2,3,4], -1))
+
+    def testNoSlices(self):
+        self.assertRaises(ValueError, splitArray([0,1,2,3,4], 0))
+
+    def testLargeSlice(self):
+        result = splitArray([0,1,2,3], 6)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], [0,1,2,3])
+
+    def testDivisibleSlices(self):
+        result = splitArray([0,1,2,3,4,5], 2)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0], [0,1,2])
+        self.assertEqual(result[1], [3,4,5])
+
+    def testIndivisibleSlices(self):
+        result = splitArray([0,1,2,3,4], 2)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0], [0,1,2])
+        self.assertEqual(result[1], [3, 4])
+
+        result = splitArray([0,1,2,3,4,5,6,7], 3)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0], [0,1,2])
+        self.assertEqual(result[1], [3,4,5])
+        self.assertEqual(result[2], [6,7])
+
+
+
+    
